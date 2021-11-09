31db1e76337a2036650c32eed1109d6371377eac
unknown <kevinh21@gmail.com>
2021-11-08 22:05:39 -0800

    First movie.app commit Day 01 take home

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+
+export const getMoviesBySearchTerm = async (searchTerm) => {
+    const url = new URL("http://www.omdbapi.com");
+    url.searchParams.append("apikey","");
+    url.searchParams.append("s",searchTerm);
+    const result = await fetch(url);
+    return  result.json();
+};
+console.log(getMoviesBySearchTerm ("Batman"));
+
+export const getMovieDetailsById = async (id) => {
+    const url = new URL("http://www.omdbapi.com");
+    url.searchParams.append("apikey","");
+    url.searchParams.append("i",id);
+    const result = await fetch(url);
+    return result.json();
+}
+console.log(getMovieDetailsById ("tt3896198"));
